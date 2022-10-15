f65c616ed3988759197234bc726a4a1c7550745d
Marvin Häuser <8659494+mhaeuser@users.noreply.github.com>
2022-10-16 01:27:28 +0200

    No persistent state in service worker

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -1,4 +1,44 @@
-const filter = {
+function configRules(url) {
+    const query_url = url || "";
+
+    if (query_url === "") {
+        browser.declarativeNetRequest.updateEnabledRulesets(
+            { disableRulesetIds: [ "block_search" ] }
+        );
+    } else {
+        browser.declarativeNetRequest.updateEnabledRulesets(
+            { enableRulesetIds: [ "block_search" ] }
+        );
+    }
+};
+
+browser.storage.sync.onChanged.addListener((changes) => {
+    if (Object.keys(changes).includes("query_url")) {
+        configRules(changes["query_url"].newValue);
+    }
+});
+
+browser.storage.sync.get(["query_url"], ({ query_url }) => {
+    configRules(query_url);
+});
+
+browser.webNavigation.onBeforeNavigate.addListener((details) => {
+    browser.storage.sync.get(["query_url"], ({ query_url }) => {
+        if (query_url === "") {
+            return
+        }
+
+        const url = new URL(details.url);
+
+        var queryKey = "q";
+        if (url.hostname.endsWith("search.yahoo.com")) {
+            queryKey = "p";
+        }
+
+        const query = encodeURIComponent(url.searchParams.get(queryKey));
+        browser.tabs.update(details.tabId, { url: query_url + query });
+    });
+}, {
     url:
     [
         {urlPrefix: "https://www.google.", pathEquals: "/search", queryPrefix: "client=safari&", queryContains: "&q=", querySuffix: "&ie=UTF-8&oe=UTF-8"},
@@ -13,60 +53,4 @@ const filter = {
         {urlPrefix: "https://www.ecosia.org/search?q=", querySuffix: "&tts=st_asaf_iphone"}
     ],
     types: ["main_frame"]
-};
-
-var query_url = "";
-
-function updateQueryUrl(url) {
-    query_url = url || "";
-
-    if (query_url === "") {
-        browser.declarativeNetRequest.updateEnabledRulesets(
-            { disableRulesetIds: [ "block_search" ] }
-            )
-    } else {
-        browser.declarativeNetRequest.updateEnabledRulesets(
-            { enableRulesetIds: [ "block_search" ] }
-            )
-    }
-};
-
-function readSettings() {
-    function cacheSettings(result) {
-        updateQueryUrl(result.query_url)
-    };
-
-    function onError(error) {
-        console.log(`Error: ${error}`);
-    };
-
-    let getting = browser.storage.sync.get("query_url");
-    getting.then(cacheSettings, onError);
-};
-
-function settingsChanged(changes) {
-    if (Object.keys(changes).includes("query_url")) {
-        updateQueryUrl(changes["query_url"].newValue)
-    }
-};
-
-function before(details) {
-    if (query_url === "") {
-        return
-    }
-
-    const url = new URL(details.url);
-
-    var queryKey = "q";
-    if (url.hostname.endsWith("search.yahoo.com")) {
-        queryKey = "p";
-    }
-
-    const query = encodeURIComponent(url.searchParams.get(queryKey));
-    browser.tabs.update(details.tabId, { url: query_url + query });
-};
-
-browser.storage.sync.onChanged.addListener(settingsChanged)
-readSettings()
-
-browser.webNavigation.onBeforeNavigate.addListener(before, filter);
+});
