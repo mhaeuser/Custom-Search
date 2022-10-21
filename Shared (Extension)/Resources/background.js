0aa60b03cfff22eaae39fa46b977aa419a1c50ca
Marvin Häuser <8659494+mhaeuser@users.noreply.github.com>
2022-10-21 22:09:53 +0200

    Use 2 spaces indentation

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -1,39 +1,39 @@
 "use strict";
 
 browser.webNavigation.onBeforeNavigate.addListener((details) => {
-    try {
-        browser.storage.sync.get(["query_url"]).then(({ query_url }) => {
-            try {
-                if ((query_url ?? "") === "") {
-                    return
-                }
+  try {
+    browser.storage.sync.get(["query_url"]).then(({ query_url }) => {
+      try {
+        if ((query_url ?? "") === "") {
+          return
+        }
 
-                const url = new URL(details.url);
-                const queryKey = url.hostname.endsWith("yahoo.com") ? "p" : "q";
-                const query = encodeURIComponent(url.searchParams.get(queryKey));
-                browser.tabs.update(details.tabId, { url: query_url + query });
-            } catch (e) {
-                console.error("%O", e);
-            }
-        });
-    } catch (e) {
+        const url = new URL(details.url);
+        const queryKey = url.hostname.endsWith("yahoo.com") ? "p" : "q";
+        const query = encodeURIComponent(url.searchParams.get(queryKey));
+        browser.tabs.update(details.tabId, { url: query_url + query });
+      } catch (e) {
         console.error("%O", e);
-    }
+      }
+    });
+  } catch (e) {
+    console.error("%O", e);
+  }
 }, {
-    url:
-    [
-        {urlPrefix: "https://www.google.", pathEquals: "/search", queryPrefix: "client=safari&", queryContains: "&q=", querySuffix: "&ie=UTF-8&oe=UTF-8"},
-        {urlPrefix: "https://www.google.", pathEquals: "/search", queryPrefix: "q=", queryContains: "&ie=UTF-8&oe=UTF-8&", querySuffix: "&client=safari"},
-        {hostEquals: "search.yahoo.com", pathEquals: "/search", queryPrefix: "ei=utf-8&fr=aaplw&p="},
-        {hostEquals: "search.yahoo.com", pathEquals: "/search", queryPrefix: "p=", querySuffix: "&fr=iphone&.tsrc=apple"},
-        {hostSuffix: ".search.yahoo.com", pathEquals: "/search", queryPrefix: "ei=utf-8&fr=aaplw&p="},
-        {hostSuffix: ".search.yahoo.com", pathEquals: "/search", queryPrefix: "p=", querySuffix: "&fr=iphone&.tsrc=apple"},
-        {urlPrefix: "https://www.bing.com/search?q=", querySuffix: "&form=APMCS1&PC=APMC"},
-        {urlPrefix: "https://www.bing.com/search?q=", querySuffix: "&form=APIPH1&PC=APPL"},
-        {urlPrefix: "https://duckduckgo.com/?q=", querySuffix: "&t=osx"},
-        {urlPrefix: "https://duckduckgo.com/?q=", querySuffix: "&t=iphone"},
-        {urlPrefix: "https://www.ecosia.org/search?q=", querySuffix: "&tts=st_asaf_macos"},
-        {urlPrefix: "https://www.ecosia.org/search?q=", querySuffix: "&tts=st_asaf_iphone"}
-    ],
-    types: ["main_frame"]
+  url:
+  [
+    {urlPrefix: "https://www.google.", pathEquals: "/search", queryPrefix: "client=safari&", queryContains: "&q=", querySuffix: "&ie=UTF-8&oe=UTF-8"},
+    {urlPrefix: "https://www.google.", pathEquals: "/search", queryPrefix: "q=", queryContains: "&ie=UTF-8&oe=UTF-8&", querySuffix: "&client=safari"},
+    {hostEquals: "search.yahoo.com", pathEquals: "/search", queryPrefix: "ei=utf-8&fr=aaplw&p="},
+    {hostEquals: "search.yahoo.com", pathEquals: "/search", queryPrefix: "p=", querySuffix: "&fr=iphone&.tsrc=apple"},
+    {hostSuffix: ".search.yahoo.com", pathEquals: "/search", queryPrefix: "ei=utf-8&fr=aaplw&p="},
+    {hostSuffix: ".search.yahoo.com", pathEquals: "/search", queryPrefix: "p=", querySuffix: "&fr=iphone&.tsrc=apple"},
+    {urlPrefix: "https://www.bing.com/search?q=", querySuffix: "&form=APMCS1&PC=APMC"},
+    {urlPrefix: "https://www.bing.com/search?q=", querySuffix: "&form=APIPH1&PC=APPL"},
+    {urlPrefix: "https://duckduckgo.com/?q=", querySuffix: "&t=osx"},
+    {urlPrefix: "https://duckduckgo.com/?q=", querySuffix: "&t=iphone"},
+    {urlPrefix: "https://www.ecosia.org/search?q=", querySuffix: "&tts=st_asaf_macos"},
+    {urlPrefix: "https://www.ecosia.org/search?q=", querySuffix: "&tts=st_asaf_iphone"}
+  ],
+  types: ["main_frame"]
 });
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,27 +1,27 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-    try {
-        browser.storage.sync.get(["query_url"]).then(({ query_url }) => {
-            try {
-                document.getElementById("query_url").value = (query_url ?? "");
-            } catch (e) {
-                console.error("%O", e);
-            }
-        });
-    } catch (e) {
+  try {
+    browser.storage.sync.get(["query_url"]).then(({ query_url }) => {
+      try {
+        document.getElementById("query_url").value = (query_url ?? "");
+      } catch (e) {
         console.error("%O", e);
-    }
+      }
+    });
+  } catch (e) {
+    console.error("%O", e);
+  }
 });
 
 document.getElementById("query_url_form").addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    try {
-        browser.storage.sync.set({
-            query_url: document.getElementById("query_url").value
-        });
-    } catch (e) {
-        console.error("%O", e);
-    }
+  try {
+    browser.storage.sync.set({
+      query_url: document.getElementById("query_url").value
+    });
+  } catch (e) {
+    console.error("%O", e);
+  }
 });
